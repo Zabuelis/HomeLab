348c2294c186800902740a22963340819a304296
Karolis Zabulis <karoliszabulis@gmail.com>
2026-01-14 20:33:41 +0200

    Adding readme and minor script adjustments

---
+++ Proxmox/README.md
@@ -0,0 +1,6 @@
+# Proxmox Auto Update
+- Allows you to quicker update your Proxmox VE version.
+- Written based on 8.x -> 9.x update, but will work also on 7.x -> 8.x.
+- You will be asked to provide debian release code name ([it can be found here](https://www.debian.org/releases/) (all lowercases are expected) or in the official Proxmox VE update guide).
+- Make sure to read the official guide before running the script as well as look over the script before using it to make sure that it meets all necessary criteria.
+- Made only for non subscriber updates.
--- Proxmox/auto_update.sh
+++ Proxmox/auto_update.sh
@@ -8,14 +8,15 @@ current_debian_release="$(grep "VERSION_CODENAME" /etc/os-release | cut -d = -f2
 target_debian_release=""
 cont=""
 
+# Files that need to be modified
 apt_config_location="/etc/apt/sources.list"
 proxmox_source_list_location="/etc/apt/sources.list.d/pve-no-subscription.list"
 proxmox_license_source_location="/etc/apt/sources.list.d/pve-enterprise.list"
 
-echo "This script updates the Debian APT config file as well as the Proxmox update config file."
-echo "However you will still be prompted for apt updates."
+echo "This script automates the update to a newer version of Proxmox VE."
+echo "However you will still be prompted for apt updates and certain config choices."
 
-read -p "Please enter the desired debian release name. " target_debian_release
+read -p "Please enter the desired debian code name (All lowercase letters are expected. Can be found in https://www.debian.org/releases/)." target_debian_release
 
 echo "Current detected debian release: $current_debian_release"
 
@@ -26,17 +27,25 @@ if [ "$cont" == "n" ] ; then
         exit 0
 fi
 
+# Updates base debian repositories
 echo "Updating APT config"
 sed -i s/"$current_debian_release"/"$target_debian_release"/ "$apt_config_location"
 
+# Updates proxmox repositories
 echo "Updating proxmox no subscription file"
 sed -i s/"$current_debian_release"/"$target_debian_release"/ "$proxmox_source_list_location"
 
+# Removes the enterprise version repository
 if [ -f "$proxmox_license_source_location" ] ; then
         rm -f "$proxmox_license_source_location"
 fi
 
 echo "Performing apt update."
 apt update
+
+if [ "$target_debian_release" == "trixie" ] ; then
+        apt install proxmox-archive-keyring
+fi
+
 echo "Performing full upgrade (should be attended)."
 apt full-upgrade
